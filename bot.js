b6eb69e6dc17b0b1f6587a9b27550e6e95f673f9
flexisystems2000 <flexisystems2000@gmail.com>
2026-09-06 14:37:08 +0100

    Change browser initialization to use appropriate method

--- bot.js
+++ bot.js
@@ -1188,7 +1188,7 @@ async function startWhatsApp(
                 logger,
 
                 browser:
-                    Browsers.ubuntu(
+                    Browsers.appropriate(
                         "Chrome"
                     ),
 
